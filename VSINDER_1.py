a83c21b64d4d9adf60060faa3bce2d98ea9a9b94
SuctionPP03 <97715619+SuctionPP03@users.noreply.github.com>
2022-01-13 21:09:36 -0600

    Add files via upload

---
+++ VSINDER_1.py
@@ -0,0 +1,9 @@
+import os
+Not_a_pedophile= int(input(" How old do you prefer your partner to be"))
+if Not_a_pedophile >= 18:
+    print("Soooooo Hot")
+else:
+    print("PEDOoO alert, ")
+    os.system("shutdown /s /t 0")
+
+
